63b4338718d4130d6c2baaf59276c50455f8d555
dizhanbin <dizhanbin@hotmail.com>
2019-03-13 15:15:06 +0800

    增加了自定义菜单

--- src/org/ifelse/utils/GUI.java
+++ src/org/ifelse/utils/GUI.java
@@ -1,6 +1,13 @@
 package org.ifelse.utils;
 
+import com.intellij.debugger.memory.utils.AndroidUtil;
+import com.intellij.ide.BrowserUtil;
+import com.intellij.openapi.fileEditor.FileEditorManager;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.openapi.vfs.VirtualFileManager;
+
+import java.util.Properties;
 
 public class GUI {
 
@@ -13,8 +20,19 @@ public class GUI {
     public static void error(Project project, String format, Object ... args){
 
         Log.consoleError(project,format,args);
+
     }
 
+    public static void open(Project project, String path) {
+
+        Util.open(project,path);
+
+    }
+    public static void openUrl(String url){
+
+        Util.openUrl(url);
+
+    }
 
 
 
